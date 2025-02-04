2a450c2dea2abb6ec851d17cf1a5c2218e2c7930
MMSRGANAPATHI <muralimarnala16@gmail.com>
2025-02-04 11:19:44 +0530

    c1

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+
+# Exit on error
+set -e  
+
 echo "Installing Rust..."
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-source $HOME/.cargo/env
-echo "Installing dependencies..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+export PATH="$HOME/.cargo/bin:$PATH"
+
+echo "Installing Python dependencies..."
 pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
